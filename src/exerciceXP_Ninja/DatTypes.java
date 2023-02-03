676fa0e691c3ad820fd523b0e2fff54a248b5f08
Magara Etchien N'gouan <magaraetchien@live.fr>
2023-02-03 17:13:36 +0000

    first commit

---
+++ src/exerciceXP_Ninja/DatTypes.java
@@ -0,0 +1,25 @@
+package exerciceXP_Ninja;
+
+public class DatTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+	
+	int age = 28;
+	double weight = 71;
+	boolean isStudent = true;
+	char gender = 'M';
+	String name = "Magara";
+	
+	
+	void displayInfo() {
+		
+		System.out.println("Your name is " + name +"\n"+ 
+		" You have a " + age + "years old" +"\n"
+				+"you are a student" + isStudent +"\n" +
+		"Your gender is " + gender);
+		
+	}
+
+}
---
+++ src/exerciceXP_Ninja/IntermediateDataType.java
@@ -0,0 +1,10 @@
+package exerciceXP_Ninja;
+
+public class IntermediateDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
